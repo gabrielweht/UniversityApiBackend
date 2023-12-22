f2e90dca48973ef8b58cd66fa6034561c1eae955
gabrielweht <gabrielestebanw@gmail.com>
2023-12-22 16:22:13 -0300

    Add Services and Controllers

---
+++ UniversityApiBackend/Services/iStudentsServices.cs
@@ -0,0 +1,14 @@
+﻿using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Services
+{
+    public interface iStudentsServices
+    {
+
+        IEnumerable<Student> GetStudentsWithCourses();
+        IEnumerable<Student> GetStudentsWithNoCourses();
+
+
+
+    }
+}
