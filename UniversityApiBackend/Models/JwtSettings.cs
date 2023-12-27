e63ea611db8baf51a7a2435925f337e140996c3f
gabrielweht <gabrielestebanw@gmail.com>
2023-12-26 22:21:46 -0300

    Added settings to JWT

---
+++ UniversityApiBackend/Models/JwtSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversityApiBackend.Models
+{
+    public class JwtSettings
+    {
+        public bool ValidateIssuerSingingKey { get; set; }
+        public string IssuerSingingKey { get; set; } = string.Empty;
+        public bool ValidateIssuer { get; set; } = true;
+        public string? ValidIssuer { get; set; }
+        public bool ValidateAudience { get; set; } = true;
+        public string? ValidAudience { get; set;}
+        public bool RequiereExpirationTime { get; set; };
+        public bool ValidateLifeTime { get; set; } = true;
+
+    }
+}
